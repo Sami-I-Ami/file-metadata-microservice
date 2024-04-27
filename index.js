42b8a14e09bf2075c9434cdc51e830caab2ada70
Sami Fink <fink.s.m.8651@gmail.com>
2024-04-27 12:32:16 +0000

    added POST and multer package

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: './public/data/uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -12,7 +14,14 @@ app.get('/', function (req, res) {
 });
 
 
-
+app.post('/api/fileanalyze', function(req, res) {
+  //output
+  res.json({
+    name: "placeholder",
+    type: "placeholder",
+    size: "placeholder"
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
